892ded7fac7a0ad3082b993bae5648c5843cddac
Michael Mansourati <mmansourati@gmail.com>
2017-04-12 23:16:02 +0000

    assignment 4 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,20 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Bon Jovi", hourly_rate: 60)
+@store1.employees.create(first_name: "jrk", last_name: "brink", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Bonko", last_name: "Changus", hourly_rate: 60)
+@store2.employees.create(first_name: "Guy", last_name: "Fieri", hourly_rate: 60)
+@store2.employees.create(first_name: "Boy", last_name: "Wonder", hourly_rate: 60)
+
 # Your code goes here ...
